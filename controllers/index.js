56e970cb6eb9c4320367d76e9c9cc07b5c098b05
Brandon Javillo <bjavillo1059@gmail.com>
2021-11-08 13:19:24 -0800

    added routes and handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
+const apiRoutes = require('./api')
+const globalRoutes = require('./global-routes');
+
+router.use('/', globalRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router
